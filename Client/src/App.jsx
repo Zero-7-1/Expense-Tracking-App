a2ef8774da3ca02237f74915e4cccbcb2c62e593
Zero-7-1 <109064402+Zero-7-1@users.noreply.github.com>
2025-01-06 16:23:59 +0530

    added routing, a pre blank page which would be login/register

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'; // Use `Navigate` instead of `Redirect`
 import { Header } from './components/Header';
 import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
@@ -6,20 +7,43 @@ import { TransactionList } from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
 
 import { GlobalProvider } from './context/GlobalState';
-
 import './App.css';
 
-function App() {
+// Blank Page Component
+function BlankPage() {
+  return <h1>This is a blank page!</h1>;
+}
+
+// Main FMA Dashboard Components
+function Dashboard() {
   return (
     <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
-        <IncomeExpenses /> 
+        <IncomeExpenses />
         <AddTransaction />
         <TransactionList />
       </div>
     </GlobalProvider>
+  );
+}
+
+// App Component
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Blank Page for '/' */}
+        <Route path="/" element={<BlankPage />} />
+
+        {/* Dashboard Page for '/dashboard' */}
+        <Route path="/dashboard" element={<Dashboard />} />
+
+        {/* Redirect any other routes to the blank page */}
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
   );
 }
 

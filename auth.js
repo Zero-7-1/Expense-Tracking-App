d85f66533407463df3ab8031624a9617de7bae74
Zero-7-1 <109064402+Zero-7-1@users.noreply.github.com>
2025-01-26 21:04:44 +0530

    secured firebase service keys

--- auth.js
+++ auth.js
@@ -1,6 +1,10 @@
-import admin from 'firebase-admin';
-import serviceAccount from './config/fma-firebase-config.json' assert { type: 'json' };
+import dotenv from 'dotenv'; 
+dotenv.config();  
 
+import admin from 'firebase-admin';
+
+// Now you can safely access your environment variables
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
